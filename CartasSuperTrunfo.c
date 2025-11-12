df591fd223957159adb3753a79680a58832ea700
Brothers021 <dallalokomatheus07@gmail.com>
2025-11-12 19:35:11 +0000

    Criei a primeira cidade.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,13 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+// primeira cidade
+char cidade1[50];
+char codigopostal1[10];
+int populacao1;
+float area1;
+float pib1;
+int pontos_turisticos1;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
